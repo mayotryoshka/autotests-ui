adf9dfd10ead489c94206dcab0be02a81f6b21b1
mayotryoshka <mother_anorexia@inbox.ru>
2026-01-22 01:43:41 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,10 @@
+def test_first_try():
+    print("Hello World!")
+
+def test_assert_positive_case():  # Новый тест, который проверяет положительный кейс
+    assert (2 + 2) == 4  # Ожидается, что тест пройдет
+    assert (3 + 3) == 6
+
+
+def test_assert_negative_case():  # Новый тест, который проверяет негативный кейс
+    assert (2 + 2) == 5, '2 + 2 != 5'  # Тут должна быть ошибка
