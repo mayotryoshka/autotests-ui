cf690e5650c5cc41a24176969ed7fe0410c6ef8e
mayotryoshka <mother_anorexia@inbox.ru>
2026-01-23 00:44:33 +0300

    Маркировки Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,8 @@
+import pytest
 from playwright.sync_api import sync_playwright, expect
 
+@pytest.mark.regression
+@pytest.mark.authorization
 def test_wrong_email_or_password_authorization():
     with sync_playwright() as playwright:
         browser_launch = playwright.chromium.launch(headless=False)  # запуск браузера в видимом режиме (headless=False)
