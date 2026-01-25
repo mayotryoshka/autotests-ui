8836812784cff8b5f2f3bd50c1e0589629713633
mayotryoshka <mother_anorexia@inbox.ru>
2026-01-25 23:38:39 +0300

    Фикстуры Pytest.Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,6 +37,7 @@ def initialize_browser_state(playwright: Playwright):
     registration_button.click()
 
     context.storage_state(path='browser-state.json')
+    browser.close()
 
 
 @pytest.fixture(scope='function')
@@ -44,4 +45,4 @@ def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
     yield context.new_page()
-    context.close()
+    browser.close()
