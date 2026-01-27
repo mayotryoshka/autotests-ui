3e12a5a5829762ebe9cc9986a4ce33583e81b273
mayotryoshka <mother_anorexia@inbox.ru>
2026-01-27 23:25:57 +0300

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,35 +1,12 @@
 import pytest
 from playwright.sync_api import sync_playwright, expect, Page, Playwright
 
+from pages.create_course_page import CreateCoursePage
+from pages.courses_list_page import CoursesListPage
+
 @pytest.mark.courses
 @pytest.mark.regression
 def test_empty_courses_list(chromium_page_with_state: Page):
-    # with sync_playwright() as playwright:
-    #    browser = playwright.chromium.launch(headless=False)
-    #    context = browser.new_context()  # для сохранения данных в локал сторадже, baseURL, размер экрана, локаль и пр.
-    #    page_new = context.new_page()
-
-    #    page_new.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
-     #   email_input = page_new.get_by_test_id('registration-form-email-input').locator('input')
-     #   email_input.fill('user.name@gmail.com')
-
-     #   username_input = page_new.get_by_test_id('registration-form-username-input').locator('input')
-     #   username_input.fill('username')
-
-     #   password_input = page_new.get_by_test_id('registration-form-password-input').locator('input')
-     #   password_input.fill('password')
-
-     #   registration_button = page_new.get_by_test_id('registration-page-registration-button')
-     #   registration_button.click()
-
-     #   context.storage_state(path='browser-state-new.json')
-
-#    with sync_playwright() as playwright:
-#        browser = playwright.chromium.launch(headless=False)
-#        context = browser.new_context(storage_state='browser-state-new.json')
-#        page_new = context.new_page()
-
         chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
         title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
@@ -46,3 +23,53 @@ def test_empty_courses_list(chromium_page_with_state: Page):
         empty_result_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
         expect(empty_result_text).to_be_visible()
         expect(empty_result_text).to_have_text('Results from the load test pipeline will be displayed here')
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(courses_list_page: CoursesListPage,
+                       create_course_page: CreateCoursePage
+                       ):
+        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+        create_course_page.check_visible_create_course_title()
+        create_course_page.check_disabled_create_course_button()
+
+        create_course_page.check_visible_image_preview_empty_view()
+
+        create_course_page.check_visible_create_course_form(
+                title='',
+                estimated_time='',
+                description='',
+                max_score='0',
+                min_score='0'
+        )
+
+        create_course_page.check_visible_exercises_title()
+        create_course_page.check_visible_create_exercise_button()
+
+        create_course_page.check_visible_exercises_empty_view()
+
+        create_course_page.upload_preview_image('./testdata/files/image.png')
+        create_course_page.check_visible_image_upload_view()
+        create_course_page.fill_create_course_form(
+                title='Playwright',
+                estimated_time='2 weeks',
+                description='Playwright',
+                max_score='100',
+                min_score='10'
+        )
+        create_course_page.click_create_course_button()
+
+        courses_list_page.check_visible_courses_title()
+        courses_list_page.check_visible_create_course_button()
+        courses_list_page.check_visible_course_card(
+                index=0,
+                title='Playwright',
+                max_score='100',
+                min_score='10',
+                estimated_time='2 weeks'
+        )
+
+
+
+
+
