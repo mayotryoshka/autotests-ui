26b9bf501f660674c3cab7afd4812986de8127bc
mayotryoshka <mother_anorexia@inbox.ru>
2026-01-30 12:55:16 +0300

    Настройки автотестов с Pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -4,7 +4,7 @@ import pytest
 from playwright.sync_api import Page, Playwright
 from pages.authentication.registration_page import RegistrationPage
 from tools.playwright.pages import initialize_playwright_page
-
+from config import settings
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Generator[Page, None, None]:
@@ -20,10 +20,13 @@ def initialize_browser_state(playwright: Playwright):
 
     registration_page = RegistrationPage(page=page_new)
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.registration_form.fill(
+        email=settings.test_user.email,
+        username=settings.test_user.username,
+        password=settings.test_user.password)
     registration_page.click_registration_button()
 
-    context.storage_state(path='browser-state.json')
+    context.storage_state(path=settings.browser_state_file)
     browser.close()
 
 
@@ -36,5 +39,5 @@ def chromium_page_with_state(
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state="browser-state.json"
+        storage_state=settings.browser_state_file
     )
--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -1,22 +1,22 @@
 import allure
 from playwright.sync_api import Playwright, Page
 from collections.abc import Generator
-
+from config import settings
 
 def initialize_playwright_page(
         playwright: Playwright,
         test_name: str,
         storage_state: str | None = None
 ) -> Generator[Page, None, None]:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state=storage_state, record_video_dir='./videos')
+        browser = playwright.chromium.launch(headless=settings.headless)
+        context = browser.new_context(storage_state=storage_state, record_video_dir=settings.video_record_dir)
         context.tracing.start(screenshots=True, snapshots=True, sources=True)
         page = context.new_page()
 
         yield page
 
-        context.tracing.stop(path=f'./tracing/{test_name}.zip')
+        context.tracing.stop(path=settings.tracing_dir.joinpath(f'{test_name}.zip'))
         browser.close()
 
-        allure.attach.file(f'./tracing/{test_name}.zip', name='trace', extension='zip')
+        allure.attach.file(path=settings.tracing_dir.joinpath(f'{test_name}.zip'), name='trace', extension='zip')
         allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
